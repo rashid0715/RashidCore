9b839565c99e70bdd95c7f068aa89eaa04bddcbf
Rashid Ansari <rashid0715@gmail.com>
2020-04-02 11:52:17 +0530

    .

---
+++ RashidCore.BookStore/RashidCore.BookStore/Controllers/Tutorials/LinqController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RashidCore.BookStore.Controllers.Tutorials
+{
+    public class LinqController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult abc()
+        {
+            return View();
+        }
+    }
+}
